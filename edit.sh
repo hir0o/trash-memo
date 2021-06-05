7bcd8cc3524e8727b0567a4158ef1c894612564a
hir0o <mk.9.q.kyu@gmail.com>
2021-06-05 18:52:31 +0900

    作成と編集

---
+++ edit.sh
@@ -0,0 +1,3 @@
+MEMO_DIR="/Users/shibuyahiroyuki/ghq/github.com/hir0o/trash-memo/memos"
+
+$EDITOR "$MEMO_DIR/`ls $MEMO_DIR | fzf`"
---
+++ main.sh
@@ -0,0 +1,4 @@
+DATETIME=`date +%Y%m%d_%H%M%S_%3N`
+MEMO_DIR="/Users/shibuyahiroyuki/ghq/github.com/hir0o/trash-memo/memos"
+
+$EDITOR "$MEMO_DIR/$DATETIME-memo.md"
