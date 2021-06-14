fc6eee1e5606680ec0d2f332b90520854e31c778
hir0o <mk.9.q.kyu@gmail.com>
2021-06-14 21:14:21 +0900

    update

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ elif [ $1 == "--delete" -o $1 == "-d" ]; then # 削除
 	rm `ls -d ${MEMO_DIR}/* | fzf --multi --preview "bat --color=always --style=header,grid {}"`
 elif [ $1 == "--new" -o $1 == "-n" ]; then # 新規作成
 	if [ $# == 1 ]; then
-		$EDITOR "$MEMO_DIR/$DATETIME-memo.md"
+		$EDITOR "$MEMO_DIR/$DATETIME.md"
 	else
 		$EDITOR "$MEMO_DIR/$2.md"
 	fi
