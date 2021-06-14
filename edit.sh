a74342a5715eccba8d664f46a4e2b6d4be6ff480
hir0o <mk.9.q.kyu@gmail.com>
2021-06-14 16:35:20 +0900

    編集と削除機能を追加

--- edit.sh
+++
@@ -1,3 +0,0 @@
-MEMO_DIR="/Users/shibuyahiroyuki/ghq/github.com/hir0o/trash-memo/memos"
-
-$EDITOR `ls -d ${MEMO_DIR}/* | fzf --preview "bat --color=always --style=header,grid {}"`
--- main.sh
+++ main.sh
@@ -1,4 +1,19 @@
-DATETIME=`date +%Y%m%d_%H%M%S_%3N`
+#! /bin/bash
+DATETIME=`date +%Y%m%d_%H%M%S`
 MEMO_DIR="/Users/shibuyahiroyuki/ghq/github.com/hir0o/trash-memo/memos"
 
-$EDITOR "$MEMO_DIR/$DATETIME-memo.md"
+if [ $# == 0 ]; then # ファイル名指定しない
+	echo "vmemo"
+elif [ $1 == "--edit" -o $1 == "-e"  ]; then # 編集
+	$EDITOR `ls -d ${MEMO_DIR}/* | fzf --preview "bat --color=always --style=header,grid {}"`
+elif [ $1 == "--delete" -o $1 == "-d" ]; then # 削除
+	rm `ls -d ${MEMO_DIR}/* | fzf --multi --preview "bat --color=always --style=header,grid {}"`
+elif [ $1 == "--new" -o $1 == "-n" ]; then # 新規作成
+	if [ $# == 1 ]; then
+		$EDITOR "$MEMO_DIR/$DATETIME-memo.md"
+	else
+		$EDITOR "$MEMO_DIR/$2.md"
+	fi
+else
+	echo "Invalid argument"
+fi
